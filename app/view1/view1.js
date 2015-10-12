6769224b4ea218ed4d4fa6725a6ea0814d11a096
tatu solla <tatu.solla@sysart.fi>
2015-10-12 19:27:57 +0300

    Search for venues at a hardcoded location

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,22 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var app = angular.module('myApp.view1', [])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'SearchController'
+        });
+    }]);
 
-.controller('View1Ctrl', [function() {
+app.controller('SearchController', function ($scope, $http) {
+    var url = "https://api.foursquare.com/v2/venues/search" +
+    "?client_id=" +
+    "&client_secret=" +
+    "&v=20130815" +
+    "&ll=40.7,-74";
 
-}]);
+    $http.get(url).success(function(data){
+       $scope.items = data;
+    });
+});
