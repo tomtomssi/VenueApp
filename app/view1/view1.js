65da5de63489c1b04b1290ca66f42bc4f867d577
tatu solla <tatu.solla@sysart.fi>
2015-10-17 21:43:31 +0300

    Added a simple test
    Checks that the controller is defined

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var app = angular.module('myApp.view1', [])
+angular.module('myApp.view1', ['ngRoute'])
 
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {
             templateUrl: 'view1/view1.html',
             controller: 'SearchController'
         });
-    }]);
+    }])
 
-app.controller('SearchController', function ($scope, $http) {
-    var url = "https://api.foursquare.com/v2/venues/search" +
-    "?client_id=" +
-    "&client_secret=" +
-    "&v=20130815" +
-    "&ll=40.7,-74";
+    .controller('SearchController', function($scope, $http) {
+        var url = "https://api.foursquare.com/v2/venues/search" +
+            "?client_id=" +
+            "&client_secret=" +
+            "&v=20130815" +
+            "&ll=40.7,-74";
 
-    $http.get(url).success(function(data){
-       $scope.items = data;
+        $http.get(url).success(function(data){
+            $scope.items = data;
+        });
     });
-});
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,16 +1,35 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.view1 module', function () {
+    var $httpBackend, $rootScope, createController, authRequestHandler;
 
-  beforeEach(module('myApp.view1'));
+    // Set up the module
+    beforeEach(module('myApp.view1'));
 
-  describe('view1 controller', function(){
+    beforeEach(inject(function ($injector) {
+        // Set up the mock http service responses
+        $httpBackend = $injector.get('$httpBackend');
+        // backend definition common for all tests
+        authRequestHandler = $httpBackend.when('GET', 'jee')
+            .respond({userId: 'userX'}, {'A-Token': 'xxx'});
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+        // Get hold of a scope (i.e. the root scope)
+        $rootScope = $injector.get('$rootScope');
+        // The $controller service is used to create instances of controllers
+        var $controller = $injector.get('$controller');
+
+        createController = function () {
+            return $controller('SearchController', {'$scope': $rootScope});
+        };
     }));
 
-  });
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('be defined', inject(function() {
+        expect(createController).toBeDefined();
+    }));
 });
